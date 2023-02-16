272bee1d94903f38e134cea19d8f57f9023437fa
huutien20 <nguyenhuutien20112002cc@gmail.com>
2023-02-16 15:10:51 +0700

    Prettier

--- src/App.js
+++ src/App.js
@@ -1,58 +1,52 @@
-import { useState } from "react";
-
-
+import { useState } from 'react';
 
 function App() {
+    const [job, setJob] = useState('');
+    const [todos, setTodo] = useState(() => {
+        const jsonJobs = JSON.parse(localStorage.getItem('jobs'));
+        return jsonJobs ?? [];
+    });
 
-  const [job, setJob] = useState('');
-  const [todos, setTodo] = useState(() => {
-    const jsonJobs = JSON.parse(localStorage.getItem('jobs'));
-    return jsonJobs ?? [];
-  });
+    const handleClick = () => {
+        setTodo((prev) => {
+            const newTodo = [...prev, job];
 
-  const handleClick = () => {
-    setTodo(prev => {
-      const newTodo = [...prev, job];
+            localStorage.setItem('jobs', JSON.stringify(newTodo));
 
-      localStorage.setItem('jobs', JSON.stringify(newTodo));
+            return newTodo;
+        });
+        setJob('');
+    };
 
-      return newTodo;
-    })
-    setJob('')
-  }
+    const handleDelete = (e) => {
+        setTodo((prev) => {
+            const newTodo = [...prev].filter((item) => e.target.name !== item);
 
-  const handleDelete = (e) => {
-    setTodo( prev => {
-      const newTodo = [...prev].filter(item => e.target.name !== item);
+            localStorage.setItem('jobs', JSON.stringify(newTodo));
 
-      localStorage.setItem('jobs', JSON.stringify(newTodo));
+            return newTodo;
+        });
+    };
 
-      return newTodo;
-    })
-  }
+    return (
+        <div className="App">
+            <input value={job} onChange={(e) => setJob(e.target.value)} />
+            <button onClick={handleClick}>Add</button>
 
-
-
-  return (
-    <div className="App">
-      <input 
-        value={job}
-        onChange= {e => setJob(e.target.value)}
-      />
-      <button onClick={handleClick}>Add</button>
-
-      <ul>
-        {todos.map((todo, index) => {
-          return (
-          <div key={index} className="item">
-            <li>{todo}</li>
-            <button name={todo} onClick={e => handleDelete(e)}>Xóa</button>
-          </div>
-          )
-        })}
-      </ul>
-    </div>
-  );
+            <ul>
+                {todos.map((todo, index) => {
+                    return (
+                        <div key={index} className="item">
+                            <li>{todo}</li>
+                            <button name={todo} onClick={(e) => handleDelete(e)}>
+                                Xóa
+                            </button>
+                        </div>
+                    );
+                })}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
