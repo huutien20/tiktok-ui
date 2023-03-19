33d653e1151f6dc13b0427c053eb99cd0a4aa06b
huutien20 <nguyenhuutien20112002cc@gmail.com>
2023-03-19 13:58:44 +0700

    Giải quyết vấn đề nhiều file index.js

---
+++ src/layouts/compoments/Search/Search.js
@@ -0,0 +1,106 @@
+import { useEffect, useState, useRef } from 'react';
+import classNames from 'classnames/bind';
+import HeadlessTippy from '@tippyjs/react/headless';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons';
+
+import { searchService } from '~/services';
+import { useDebounce } from '~/hooks';
+import { Wrapper as PopperWrapper } from '~/compoments/Popper';
+import AccountItem from '~/compoments/AccountItem';
+import { SearchIcon } from '~/compoments/Icons';
+import styles from './Search.module.scss';
+
+const cx = classNames.bind(styles);
+
+function Search() {
+    const [searchValue, setSearchValue] = useState('');
+    const [searchResult, setSearchResult] = useState([]);
+    const [showResult, setShowResult] = useState(true);
+    const [loading, setLoading] = useState(false);
+
+    const debounce = useDebounce(searchValue, 500);
+
+    const inputRef = useRef();
+
+    useEffect(() => {
+        if (!debounce.trim()) {
+            setSearchResult([]);
+            return;
+        }
+
+        const fetchApi = async () => {
+            setLoading(true);
+
+            const result = await searchService(debounce);
+            setSearchResult(result);
+            setLoading(false);
+        };
+        fetchApi();
+    }, [debounce]);
+
+    const handleHideResult = () => {
+        setShowResult(false);
+    };
+
+    const handleClear = () => {
+        setSearchValue('');
+        setSearchResult([]);
+        inputRef.current.focus();
+    };
+
+    const handleChange = (e) => {
+        const searchVal = e.target.value;
+        if (!searchVal.startsWith(' ')) {
+            setSearchValue(e.target.value);
+        }
+    };
+
+    return (
+        //Using a wrapper <div> tag around the reference element solves this
+        //by creating a new parentNode context.
+        <div>
+            <HeadlessTippy
+                interactive
+                visible={showResult && searchResult.length > 0}
+                render={(attrs) => (
+                    <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                        <PopperWrapper>
+                            <h4 className={cx('search-title')}>Accounts</h4>
+                            <div className={cx('search-body')}>
+                                {searchResult.map((result) => (
+                                    <AccountItem key={result.id} data={result} />
+                                ))}
+                            </div>
+                        </PopperWrapper>
+                    </div>
+                )}
+                onClickOutside={handleHideResult}
+            >
+                <div className={cx('search')}>
+                    <input
+                        ref={inputRef}
+                        value={searchValue}
+                        placeholder="Search accounts and videos"
+                        spellCheck={false}
+                        onChange={handleChange}
+                        onFocus={() => setShowResult(true)}
+                    />
+
+                    {!!searchValue && !loading && (
+                        <button className={cx('clear')} onClick={handleClear}>
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
+                    )}
+                    {loading && <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />}
+
+                    <button className={cx('search-btn')} onMouseDown={(e) => e.preventDefault()}>
+                        <SearchIcon width="24px" height="24px" />
+                    </button>
+                </div>
+            </HeadlessTippy>
+        </div>
+    );
+}
+
+export default Search;
